d70feb340db098eac0886c944b46d2cc6062c333
Priyank Agarwal <priyankagarwal00.pa@gmail.com>
2019-12-26 10:53:34 +0530

    Simple CLass Practice

---
+++ Class_Practice1.py
@@ -0,0 +1,11 @@
+# Create empty class: DataShell
+class Datashell:
+  
+    # Pass statement
+    pass
+
+# Instantiate DataShell: my_data_shell
+my_data_shell = Datashell()
+
+# Print my_data_shell
+print(my_data_shell)
