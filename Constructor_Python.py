83863f29c4cbef2ff87d950644426941afe21f05
Priyank Agarwal <priyankagarwal00.pa@gmail.com>
2019-12-26 11:09:26 +0530

    Constructor Practice

---
+++ Constructor_Python.py
@@ -0,0 +1,14 @@
+# Create class: DataShell
+class Datashell:
+  
+	# Initialize class with self argument
+    def __init__(self):
+      
+        # Pass statement
+        pass
+
+# Instantiate DataShell: my_data_shell
+my_data_shell = Datashell()
+
+# Print my_data_shell
+print(my_data_shell)
