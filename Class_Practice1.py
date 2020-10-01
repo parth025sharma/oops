d0356827ed7f1eeadc400a250f5b6473a9db2ff9
parth025sharma <parthsharma06960@gmail.com>
2020-10-01 16:32:55 +0530

    Hacktoberfest commit

--- Class_Practice1.py
+++ Class_Practice1.py
@@ -4,8 +4,8 @@ class Datashell:
     # Pass statement
     pass
 
-# Instantiate DataShell: my_data_shell
+# Instantiating DataShell: my_data_shell
 my_data_shell = Datashell()
 
-# Print my_data_shell
+# Printing my_data_shell
 print(my_data_shell)
